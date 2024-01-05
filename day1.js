d2874be7b0b2facfc8a090c4921f52db1766392d
mradulgarg <mridul.hnd@gmail.com>
2024-01-05 15:11:35 +0530

    add day1 file

---
+++ day1.js
@@ -0,0 +1,76 @@
+
+// Problem 1
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+let school = "Masai School" ;
+let education = "A Transformation in Education" ;
+console.log(school)
+console.log(education)
+
+// Problem 2
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let myname = "Mradul Garg";
+let fathername = "Mr. Mahipal Garg";
+let mothername = "Mrs. Anita Garg";
+
+console.log(myname)
+console.log(fathername)
+console.log(mothername)
+
+// Problem 3
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Concatenate and console them in a new string to get the output like this : ("My name is"+ name +"and my age is"+ age +".")
+
+let Lname = "rahul";
+let age = 24;
+
+console.log(Lname, age)
+console.log(typeof(Lname),  typeof(age))
+
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+console.log(" __________________________________________________")
+console.log("|                                                  |")
+console.log(`|       🎉  🅼 🅰  🆂 🅰  🅸  🆂  🅲  🅷 🅾  🅾  🅻  🎉       |`)
+console.log("|__________________________________________________|")
+console.log("|__________________________________________________|")
+console.log(`|                                                  |
+|              █▀█ █▀▀ █▀ █░█ █░░ ▀█▀              |
+|              █▀▄ ██▄ ▄█ █▄█ █▄▄ ░█░              |   `)                 
+console.log("|__________________________________________________|")
+console.log("|__________________________________________________|")
+console.log("|                        ||                        |")
+console.log("|  NAME :-> Rahul Arora  ||   Roll No :-> 717      |")
+console.log("|                        ||                        |")
+console.log("|  Grade :-> 12th        ||   Section :-> C        |")
+console.log("|                        ||                        |")
+console.log("|________________________||________________________|")
+console.log("|__________________________________________________|")
+console.log("|           |             |         |              |")
+console.log("|    No.    |   Subjects  |  Marks  |  Grade       |")
+console.log("|___________|_____________|_________|______________|")
+console.log("|           |             |         |              |")
+console.log("|     1     |    Maths    |   150   |     B        |")
+console.log("|           |             |         |              |")
+console.log("|     2     |   Science   |   120   |     C        |")
+console.log("|           |             |         |              |")
+console.log("|     3     |   Computer  |   180   |     A        |")
+console.log("|___________|_____________|_________|______________|")
+console.log("|__________________________________________________|")
+console.log("|                                                  |")
+console.log("|**************************************************|")
+console.log("|                                                  |")
+console.log("|     4          Total        450         B        |")
+console.log("|                                                  |")
+console.log("|**************************************************|")
+console.log("|__________________________________________________|")
