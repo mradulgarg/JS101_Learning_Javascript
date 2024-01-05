f197bfa23ca36003babc91d1b186d0baf76b15a4
mradulgarg <mridul.hnd@gmail.com>
2024-01-05 15:24:16 +0530

    add day2 file

---
+++ day2.js
@@ -0,0 +1,16 @@
+// problem 1
+console.log("Masai School");
+console.log("A Transformation in Education");
+
+// problem 2
+multiplyBy50(number)
+console.log(number*50);
+
+
+// problem 3
+let prod = one * two * three * four * five * six ;
+console.log(prod);
+
+// problem 4
+let sum = one + two + three + four + five;
+console.log(sum);
